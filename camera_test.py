1f71d8ed1eb97dae87374fa70e87df88835c2052
Mo Zhu <mo.zhu.th@dartmouth.edu>
2018-12-06 12:07:26 -0500

    responds to button press

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,21 @@
 from picamera import PiCamera
-from time import sleep
+from RPi import GPIO
+import time
 
 camera = PiCamera()
 
-camera.start_preview()
-sleep(10)
-camera.stop_preview()
+button = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button, GPIO.IN)
+
+try:
+    while True:
+        if GPIO.input(button) == 0:
+            print "Button pressed!"
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    pass
+
+GPIO.cleanup()
+
