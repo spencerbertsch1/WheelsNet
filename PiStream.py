7ef646eb0ac3b2357bdf61ebd72fdad00f6eb209
Mo Zhu <mo.zhu.th@dartmouth.edu>
2018-12-07 15:12:18 -0500

    reorganize code

--- PiStream.py
+++ PiStream.py
@@ -1,5 +1,4 @@
-# Dartmouth College, Winter 2019
-
+# Dartmouth College, Winter 2018/2019
 
 # --------------------------- IMPORTS --------------------------- #
 from picamera import PiCamera
@@ -8,53 +7,89 @@ import RPi.GPIO as GPIO
 import time
 import web
 import subprocess
-camera = PiCamera()
-camera.resolution = (720, 720)
-led = 5
-button = 24
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(led, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+from google.oauth2 import service_account
+from google.cloud import vision
+from google.cloud.vision import types
+import io
+import time
+
+from lcd import LCD
+
+#RUNS AT BEGINNING 
+start_server = "mkdir /tmp/stream; raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &"
+subprocess.call(start_server, shell = True)
+
+start_stream = "LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i \"input_file.so -f /tmp/stream -n pic.jpg\" -o \"output_http.so -w /usr/local/www\" &"
+subprocess.call(start_stream, shell = True)
+
 
 #Commands to start and stop stream
-start_server_command = "raspistill -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0"
+#start_server_command = "raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0"
+start_stream_command = "LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i \"input_file.so -f /tmp/stream -n pic.jpg\" -o \"output_http.so -w /usr/local/www\" &"
 kill_server_command = "pkill raspistill"
+kill_stream_command = "pkill mjpg_streamer"
 stream_flag = 0
 
+display = LCD()
+creds = service_account.Credentials.from_service_account_file('./.google-cloud-creds.json')
+client = vision.ImageAnnotatorClient(credentials=creds)
 
 # --------------------------- FUNCTIONS --------------------------- #
-def start_stream():
-    print("Starting stream...")
-    global stream_flag
-    stream_flag = 1
-    camera.start_preview(fullscreen = False, alpha = 0, window = (0, 0, 720, 720))
-    subprocess.call(start_server_command, shell = True) 
-
+#def start_stream():
+    #print("Starting stream...")
+    #global stream_flag
+    #stream_flag = 1
+    #camera.rotation = 180
+    #camera.start_preview(fullscreen = False, window = (0, 0, 720, 720))
+    #subprocess.call(start_server_command, shell = True)
+    #subprocess.call(start_stream_command, shell = True)
+    
 def end_stream():
     print("Ending stream...")
     stream_flag = 1
-    subprocess.call(kill_server_command, shell = True) 
-    camera.stop_preview()
+    subprocess.call(kill_stream_command, shell = True)
+    subprocess.call(kill_server_command, shell = True)
+    #camera.stop_preview()
 
-def take_picture():
-    print("Taking picture...")
-    camera.start_preview(fullscreen = False, window = (0, 0, 720, 720))
-    camera.capture('/home/pi/Desktop/testresize.jpg', resize = (360, 360))
-    camera.capture('/home/pi/Desktop/test.jpg')
+def recognize():
+    display.clear()
+    display.display_message("Recognizing...", row=0)
+
+    with io.open("/tmp/stream/pic.jpg", 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content = content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    for label in labels:
+        display.clear()
+        display.display_message(label.description, row=0)
+        display.display_message(" (" + str(round(label.score * 100)) + "%)", row=1)
+        time.sleep(2)
+    
+    display.default_message()
+
+    #NOT STARTING STREAM AGAIN
+    #camera.rotation = 180
+    #camera.start_preview(fullscreen = False, window = (0, 0, 720, 720))
+    #camera.capture('/home/pi/Desktop/testresize.jpg', resize = (360, 360))
+    #camera.capture('/home/pi/Desktop/test.jpg')
 
 # --------------------------- WEB --------------------------- #
 try: 
     web.server_start()
-    web.arbitrary_html( "<br/>" )
     web.arbitrary_html( "<img src='https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fcbsnews1.cbsistatic.com%2Fhub%2Fi%2Fr%2F2013%2F04%2F24%2F5e4d566c-c41a-11e2-a43e-02911869d855%2Fthumbnail%2F620x350%2Ffbec6f2b9675ed568f6dc091e178c4c2%2Fdartmouth_college_generic_1088711_fullwidth.jpg&f=1'> </img>" )
     
-    web.register( "<button>Start Stream</button>", start_stream )
-    web.register( "<button>End Stream</button>", end_stream )
-    web.register( "<button>Take Picture</button>", take_picture )
-
-
     #Display Stream
-    
+    web.arbitrary_html( "<br/>" )
+    web.arbitrary_html( "<img src='http://10.81.11.186:8080/?action=stream'> </img>" )
+
+    #Buttons
+    web.arbitrary_html( "<br/>" )
+    web.register( "<button>End Stream</button>", end_stream )
+    web.register( "<button>Recognize!!</button>", recognize )
 
     while True:
         A=1
@@ -63,5 +98,9 @@ except:
     pass
 
 # the program is finished, we put things back in their original state
-print ("> finishing")
+print ("> finishing...")
+#camera.close()
+subprocess.call(kill_stream_command, shell = True)
+subprocess.call(kill_server_command, shell = True)
 web.server_stop()
+
--- README.md
+++ README.md
@@ -22,3 +22,28 @@ Run on the Raspberry Pi
 3. in the interface, go to "Interfacing Options"
 4. turn on Camera and LCD
 5. run `reboot` in your terminal
+
+# Streaming
+
+For this project, a Raspberry Pi 3b+ was used as a server to stream video from an onboard camera to a remote host.
+
+Code in this folder was used to allow the pi to stream live video to another machine. 
+
+# Streaming Installation
+
+SOURCE: https://blog.miguelgrinberg.com/post/how-to-build-and-run-mjpg-streamer-on-the-raspberry-pi
+
+This information was included into our python program using "subprocess.call", but it's always good to remember exactly how the stream itself works. 
+
+NOTE: Make sure that "Web.py" is stored in the same directory as the below python files being run!!
+
+
+-------------- STEPS --------------
+
+1. Open Terminal and enter: raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0
+
+2. Open a new window in the terminal and enter: LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"
+
+3. Watch the stream from another computer in your network by using -  http://<IP-address>:8080
+
+4. Kill the stream by opening a new terminal and entering: pkill raspistill
